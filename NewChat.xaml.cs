efb5abcd35fbd8a16d7401b590f900c31c3f5e69
vvvvvvvector <57532024+vvvvvvvector@users.noreply.github.com>
2020-09-14 16:02:29 +0300

    first commit

---
+++ NewChat.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Globalization;
+
+namespace Chat
+{
+    /// <summary>
+    /// Interaction logic for Chat.xaml
+    /// </summary>
+    public partial class NewChat : Window
+    {
+        Socket accept;
+        Socket socket;
+        bool _isHost;
+        public NewChat(bool isHost, string Ip = null)
+        {
+            InitializeComponent();
+
+            if (isHost)
+            {
+                _isHost = true;
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                socket.Bind(new IPEndPoint(IPAddress.Any, 8005));
+                socket.Listen(0);
+                ChatBox.Text = "Server Started...\nWaiting for incoming client connections...";
+
+                Task.Factory.StartNew(() =>
+                {
+                    accept = socket.Accept();
+
+                    Dispatcher.Invoke(() =>
+                    {
+                        ChatBox.Text += "\nConnection accepted!";
+                    });
+
+                    while (true)
+                    {
+                        try
+                        {
+                            var buffer = new byte[256];
+                            var receivedData = accept.Receive(buffer, 0, buffer.Length, 0);
+
+                            if (receivedData <= 0)
+                            {
+                                throw new SocketException();
+                            }
+
+                            Array.Resize(ref buffer, receivedData);
+
+                            Dispatcher.Invoke(() =>
+                            {
+                                ChatBox.Text += $"\n{DateTime.Now.ToShortTimeString()} Client: {Encoding.Default.GetString(buffer)}";
+                            });
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            socket.Shutdown(SocketShutdown.Both);
+                            socket.Close();
+                            Application.Current.Shutdown();
+                        }
+                    }
+                });
+
+            }
+            else
+            {
+                _isHost = false;
+                socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    socket.Connect(new IPEndPoint(IPAddress.Parse(Ip), 8005));
+                    ChatBox.Text += $"Your are connected to: {Ip}";
+                
+                    Task.Factory.StartNew(() =>
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                var buffer = new byte[256];
+                                var receivedData = socket.Receive(buffer, 0, buffer.Length, 0);
+
+                                if (receivedData <= 0)
+                                {
+                                    throw new SocketException();
+                                }
+
+                                Array.Resize(ref buffer, receivedData);
+
+                                Dispatcher.Invoke(() =>
+                                {
+                                    ChatBox.Text += $"\n{DateTime.Now.ToShortTimeString()} Host: {Encoding.Default.GetString(buffer)}";
+                                });
+                            }
+                            catch (Exception ex)
+                            {
+                                MessageBox.Show(ex.Message);
+                            }
+                        }
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    socket.Close();
+                    Application.Current.Shutdown();
+                }
+            }
+        }
+
+        void SendButton(object sender, RoutedEventArgs e)
+        {
+            var dataToSend = Encoding.Default.GetBytes(BoxForMessage.Text);
+            if (_isHost)
+            {
+                accept.Send(dataToSend, 0, dataToSend.Length, 0);
+                ChatBox.Text += $"\n{DateTime.Now.ToShortTimeString()} Host: {BoxForMessage.Text}";
+            }
+            else
+            {
+                socket.Send(dataToSend, 0, dataToSend.Length, 0);
+                ChatBox.Text += $"\n{DateTime.Now.ToShortTimeString()} Client: {BoxForMessage.Text}";
+            }
+        }
+
+        void BoxForMessage_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            textBox.Text = "";
+            textBox.GotFocus -= BoxForMessage_GotFocus;
+        }
+    }
+}
---
+++ StartWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Shell;
+
+namespace Chat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class StartWindow : Window
+    {
+        public StartWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton(object sender, RoutedEventArgs e)
+        {
+            if (Regex.IsMatch(IpBox.Text, @"^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"))
+            {
+                var clientWindow = new NewChat(false, IpBox.Text);
+                //this.Visibility = Visibility.Hidden;
+                //this.Hide();
+                clientWindow.ShowDialog();
+            }
+            else 
+            {
+                MessageBox.Show("It's not an IP. Try one more time.");
+            }
+        }
+
+        private void HostButton(object sender, RoutedEventArgs e)
+        {
+            var hostWindow = new NewChat(true);
+            //this.Hide();
+            hostWindow.ShowDialog();
+        }
+
+        private void IpBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            textBox.Text = "";
+            textBox.GotFocus -= IpBox_GotFocus;
+        }
+    }
+}
